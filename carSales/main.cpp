a97db1fddb30a71252ae7c034d38ef05a687a751
Miguel A Basilio <lokoangel06@gmail.com>
2021-05-02 20:55:58 -0700

    Update main.cpp

--- carSales/main.cpp
+++ carSales/main.cpp
@@ -14,12 +14,6 @@ using namespace std;
 
 int main(int argc, const char * argv[]) {
     
- 
-    cout << "Use the Car Class: \n\n";
-    CarSales Chevrolet (2011, "Camaro",0);
-    CarSales Mazda (2021, "Miata",0);
-    CarSales Honda (2001, "Accord",0);
-    CarSales Ford (2010, "Focus", 0);
     
     //this piece of code reads a file with differen cars and adds the speed and brakes aswell:
    
@@ -39,8 +33,7 @@ int main(int argc, const char * argv[]) {
         cout <<"Accelarating..."<< endl;
         for (int faster = 0; faster <= 5; faster++)
         {
-           // Chevrolet.acelerate();
-            //cout <<Chevrolet.getYear()<<" "<<Chevrolet.getMake()<< "Current Speed: " << Chevrolet.getSpeed() <<" mph."<< endl;
+          
             curCar.acelerate();
             cout << curCar.getYear()<< " " << setw(14)<< curCar.getMake() << " " << setw(4)<<curCar.getSpeed()<< " " << endl;
          
@@ -51,8 +44,7 @@ int main(int argc, const char * argv[]) {
         
         for (int slower = 0; slower <= 5; slower++)
         {
-           // Chevrolet.brake();
-          //  cout <<Chevrolet.getYear()<<" " <<Chevrolet.getMake()<< "Current Speed: " << Chevrolet.getSpeed() <<" mph."<< endl;
+        
             curCar.brake();
             cout << curCar.getYear()<< " " << setw(14)<< curCar.getMake() << " " <<setw(4)<< curCar.getSpeed()<< " " << endl;
         }
